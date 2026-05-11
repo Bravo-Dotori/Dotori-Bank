fd35b0f9e7770362884bf474f2ed585c2a387c14
hanyiseul <seulli_hys@daum.net>
2026-05-11 11:40:08 +0900

    회원가입/로그인 기능 구현 중

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,28 +1,20 @@
-// userModels.js : 사용자 모델 정의
-
-const pool = require("../db"); // db 연결 객체
+const pool = require("../db");
 
 // 회원가입
 exports.signup = async (name, user_id, pwd) => {
-  // 실행할 sql문
-  // user 테이블에 입력값(valuse (?)부분) 삽입(insert)
   const sql = `
     insert into users (name, user_id, pwd)
     values (?,?,?)
   `
-
-  // pool에서 커넥션 하나 가져와서 해당 sql 실행  
   const [rows] = await pool.query(sql, [name, user_id, pwd]);
-
   return rows;
 }
 
-// 로그인, 아이디 중복 체크
-exports.checkId = async(user_id) => {
+// 로그인
+exports.login = async(user_id) => {
   const sql = `
     select * from users where user_id=?
   `
-  
   const [rows] = await pool.query(sql, [user_id]);
   
   return rows;
