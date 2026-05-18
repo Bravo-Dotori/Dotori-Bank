103a5912ee31fb90900637010333005e6f321495
hanyiseul <seulli_hys@daum.net>
2026-05-18 11:54:03 +0900

    받는 계좌 조회 api

--- server/swagger/account.swagger.js
+++ server/swagger/account.swagger.js
@@ -286,3 +286,130 @@
  *                   type: string
  *                   example: 계좌 생성 서버 에러
  */
+
+/**
+ * @swagger
+ * /api/accounts/toAccount:
+ *   post:
+ *     summary: 받는 계좌 조회
+ *     tags: [Accounts]
+ *     security:
+ *       - bearerAuth: []
+ *
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               account_number:
+ *                 type: string
+ *                 example: 100-5958-120061
+ *
+ *               name:
+ *                 type: string
+ *                 example: 홍길동
+ *
+ *     responses:
+ *       200:
+ *         description: 계좌 조회 성공
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *
+ *                 message:
+ *                   type: string
+ *                   example: 계좌 조회 성공
+ *
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *
+ *                       user_id:
+ *                         type: integer
+ *                         example: 1
+ *
+ *                       account_number:
+ *                         type: string
+ *                         example: 100-1234-567890
+ *
+ *                       account_type:
+ *                         type: string
+ *                         example: demand
+ *
+ *                       balance:
+ *                         type: integer
+ *                         example: 3000000
+ *
+ *                       is_active:
+ *                         type: boolean
+ *                         example: true
+ *
+ *                       name:
+ *                         type: string
+ *                         example: 홍길동
+ *
+ *       400:
+ *         description: 필수값 누락
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *
+ *                 message:
+ *                   type: string
+ *                   example: 필수값 누락
+ *
+ *       404:
+ *         description: 계좌 정보 없음
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *
+ *                 errorCode:
+ *                   type: string
+ *                   example: ACCOUNT_NOT_FOUND
+ *
+ *                 message:
+ *                   type: string
+ *                   example: 계좌 정보 없음
+ *
+ *       500:
+ *         description: 서버 오류
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *
+ *                 errorCode:
+ *                   type: string
+ *                   example: ACCOUNT_SERVER_ERROR
+ *
+ *                 message:
+ *                   type: string
+ *                   example: 계좌 조회 서버 에러
+ */
