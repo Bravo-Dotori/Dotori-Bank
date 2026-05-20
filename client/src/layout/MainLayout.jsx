260f549b9ad0976268c2d062ea80317b5a6d167d
hanyiseul <seulli_hys@daum.net>
2026-05-20 15:27:01 +0900

    React.lazy 및 Suspense 기반 코드 스플리팅 적용

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,41 +1,51 @@
 import { Outlet, useLocation } from 'react-router-dom';
+import { Suspense, useRef, useState } from 'react';
 
-import styles from './layout.module.css'
+import styles from './layout.module.css';
 
 import Gnb from '@/components/topbar/Gnb';
 import Snb from '@/components/sidebar/Snb';
-import { useRef, useState } from 'react';
+
+import StatusCard from '@/components/card/StatusCard/StatusCard';
 
 const MainLayout = () => {
-  const location = useLocation();
+    const location = useLocation();
 
-  const hideSnbPaths = ['/login', '/signup', '/admin'];
+    const hideSnbPaths = ['/login', '/signup', '/admin'];
 
-  const isHideSnb = hideSnbPaths.includes(location.pathname);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const menuBtnRef = useRef(null);
+    const isHideSnb = hideSnbPaths.includes(location.pathname);
 
-  return (
-    <div className={styles.layout}>
-      <header>
-            <Gnb
-                isMenuOpen={isMenuOpen}
-                setIsMenuOpen={setIsMenuOpen}
-                menuBtnRef={menuBtnRef}
-            />
-      </header>
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-      <main className={isHideSnb ? styles.main : styles.mainLayout}>
-            <Snb
-                isMenuOpen={isMenuOpen}
-                setIsMenuOpen={setIsMenuOpen}
-                menuBtnRef={menuBtnRef}
-                isHideSnb={isHideSnb}
-            />
-            <Outlet />
-      </main>
-    </div>
-  );
+    const menuBtnRef = useRef(null);
+
+    return (
+        <div className={styles.layout}>
+            <header>
+                <Gnb
+                    isMenuOpen={isMenuOpen}
+                    setIsMenuOpen={setIsMenuOpen}
+                    menuBtnRef={menuBtnRef}
+                />
+            </header>
+
+            <main className={isHideSnb ? styles.main : styles.mainLayout}>
+                <Snb
+                    isMenuOpen={isMenuOpen}
+                    setIsMenuOpen={setIsMenuOpen}
+                    menuBtnRef={menuBtnRef}
+                    isHideSnb={isHideSnb}
+                    className={
+                        isHideSnb ? styles.hide : ''
+                    }
+                />
+
+                <Suspense fallback={<StatusCard title="잠시만 기다려주세요" />}>
+                    <Outlet />
+                </Suspense>
+            </main>
+        </div>
+    );
 };
 
 export default MainLayout;
